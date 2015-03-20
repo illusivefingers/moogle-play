98f9202d74d3e62ff80c67f974c78b8c2c257f65
Nathan Norton <nathan@9gs.org>
2015-03-20 14:05:13 -0600

    Initial commit

---
+++ .env.environment.sample.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+	'APP_BASE_URL'      => '',
+
+	'DEBUG'             => '',
+
+	'TIMEZONE'          => 'America/Boise',
+
+	'MYSQL_DB'          => '',
+	'MYSQL_HOST'        => '',
+	'MYSQL_USER'        => '',
+	'MYSQL_PASSWORD'    => '',
+
+	'SMTP_HOST'         => '',
+	'SMTP_PORT'         => '',
+	'STMP_USER'         => '',
+	'SMTP_PASS'         => '',
+	'SMTP_FROM'         => '',
+	'SMTP_NAME'         => '',
+	'MAIL_PRETEND'      => true,
+
+	'RANDOM_KEY'        => 'UlBF3LLP0F9n5FaMWlRejQ3K4MLO4oo4'
+
+];
---
+++ environment.sample.php
@@ -0,0 +1,3 @@
+<?php
+
+return 'some-environment-name';
